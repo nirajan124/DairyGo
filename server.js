3eac78dff7fe1d26844b38619a9a3d047d61c963
nirajan124 <nirajanbhattarai20@gmail.com>
2025-07-31 00:41:33 +0545

    Add backend API project

---
+++ server.js
@@ -0,0 +1,98 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize"); // for sql injection
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.options("*", cors());
+// Load env file
+dotenv.config({
+    path: "./config/config.env",
+});
+
+// Connect to database
+connectDB();
+
+// Route files
+const auth = require("./routes/customer");
+const adminAuth = require("./routes/AuthRoute");
+const customerRoutes = require("./routes/customer");
+const contactRoutes = require("./routes/ContactRoute");
+
+const product = require("./routes/ProductRoute");
+const wishlist = require("./routes/WishlistRoute");
+const khaltiRoutes = require("./routes/KhaltiRoute");
+const booking = require("./routes/BookingRoute");
+const reviewRoutes = require("./routes/ReviewRoute");
+
+// Body parser
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    next();
+});
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+// Serve uploads folder for product images
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+
+// app.use(express.static('public'));
+
+// Mount routers
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/admin-auth", adminAuth);
+app.use("/api/v1/customers", customerRoutes);
+app.use("/api/v1/contact", contactRoutes);
+app.use("/api/v1/products", product);
+app.use("/api/v1/wishlist", wishlist);
+app.use("/api/khalti", khaltiRoutes);
+app.use("/api/v1/bookings", booking);
+app.use("/api/v1/reviews", reviewRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
